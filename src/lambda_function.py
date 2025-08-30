94da64132f6bd8e5b0b5670d4a63f92f8740d078
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2025-08-30 10:16:00 -0300

    Update lambda response

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,5 +3,5 @@ from typing import Dict, Any
 def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     return {
         "statusCode": 200,
-        "message": "Success"
+        "body": "Success"
     }
