97f3d3c9affe61107dd9125a471bdfe88db6e19b
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2025-08-29 23:47:20 -0300

    Add type hints

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,4 +1,6 @@
-def lambda_handler(event, context):
+from typing import Dict, Any
+      
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     return {
         "statusCode": 200,
         "message": "Success"
